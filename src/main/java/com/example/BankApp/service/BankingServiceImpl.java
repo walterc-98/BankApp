e69391b07187a6ad5a35cd047c4fe6a0b4794e67
walterc98 <walter.condori.10@gmail.com>
2025-12-16 23:24:17 -0800

    adding BankingServiceImpl.java and its methods

---
+++ src/main/java/com/example/BankApp/service/BankingServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.BankApp.service;
+
+import com.example.BankApp.model.BankAccount;
+import com.example.BankApp.model.CheckingAccount;
+import com.example.BankApp.repository.BankAccountRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class BankingServiceImpl implements BankingService{
+
+    private final BankAccountRepository bankAccountRepository;
+
+    public BankingServiceImpl(BankAccountRepository bankAccountRepository) {
+        this.bankAccountRepository = bankAccountRepository;
+    }
+
+    @Override
+    public void createAccount(CheckingAccount checkingAccount) {
+        bankAccountRepository.save(checkingAccount);
+    }
+
+    @Override
+    public void deposit(String accountId, BigDecimal amount) {
+        BankAccount bankAccount = bankAccountRepository.findById(accountId)
+                .orElseThrow(()-> new IllegalArgumentException("Account not found"));
+        bankAccount.deposit(amount);
+        bankAccountRepository.save(bankAccount);
+    }
+
+    @Override
+    public void withdraw(String accountId, BigDecimal amount) {
+        BankAccount bankAccount= bankAccountRepository.findById(accountId)
+                .orElseThrow(()-> new IllegalArgumentException("Account not found"));
+        bankAccount.withdraw(amount);
+        bankAccountRepository.save(bankAccount);
+    }
+
+    @Override
+    public BigDecimal getBalance(String accountId) {
+        BankAccount bankAccount = bankAccountRepository.findById(accountId)
+                .orElseThrow(()-> new IllegalArgumentException("Account not found"));
+        return bankAccount.getBalance();
+    }
+}
