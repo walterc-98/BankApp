56d7dc4c04125a8aa7f0df4a1fe40021061e83a0
walterc98 <walter.condori.10@gmail.com>
2025-12-16 22:44:54 -0800

    adding methods to BankingService.java

--- src/main/java/com/example/BankApp/service/BankingService.java
+++ src/main/java/com/example/BankApp/service/BankingService.java
@@ -1,4 +1,16 @@
 package com.example.BankApp.service;
 
+import com.example.BankApp.model.CheckingAccount;
+
+import java.math.BigDecimal;
+
 public interface BankingService {
+
+    void createAccount(CheckingAccount checkingAccount);
+
+    void deposit(String accountId, BigDecimal amount);
+
+    void withdraw(String accountId, BigDecimal amount);
+
+    BigDecimal getBalance(String accountId);
 }
