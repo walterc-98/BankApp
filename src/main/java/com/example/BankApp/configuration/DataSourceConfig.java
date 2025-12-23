f680c3761fb660014d56c85a0b19c825d0b9b34f
walterc98 <walter.condori.10@gmail.com>
2025-12-22 22:20:05 -0800

    adding datasource method

--- src/main/java/com/example/BankApp/configuration/DataSourceConfig.java
+++ src/main/java/com/example/BankApp/configuration/DataSourceConfig.java
@@ -1,4 +1,20 @@
 package com.example.BankApp.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class DataSourceConfig {
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("org.postgresql.Driver");
+        ds.setUrl("jdbc:postgresql://localhost:5432/banking_app");
+        ds.setUsername("user");
+        ds.setPassword("");
+        return ds;
+    }
 }
